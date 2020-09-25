c27791e88ab6e9eb44335f668f2218b7533db8bf
kchiu-dev <70428366+kchiu-dev@users.noreply.github.com>
2020-09-25 19:11:49 -0400

    Add axios environment support

--- client/api/build-client.js
+++ client/api/build-client.js
@@ -1,17 +1,28 @@
 import axios from "axios";
+import https from "https";
 
 export default ({ req }) => {
   if (typeof window === "undefined") {
     // We are on the server
-
-    return axios.create({
-      baseURL:
-        // Local Cluster
-        // "http://ingress-nginx-controller.ingress-nginx.svc.cluster.local",
-        // Production Cluster
-        "http://www.ticketing-app.tk",
-      headers: req.headers,
-    });
+    if (process.env.NEXT_PUBLIC_ENV === "staging") {
+      return axios.create({
+        // Deal with SSL certificate issues
+        httpsAgent: new https.Agent({
+          rejectUnauthorized: false,
+        }),
+        baseURL:
+          // Local Cluster
+          "https://ticketing-app-dev.tk",
+        headers: req.headers,
+      });
+    } else {
+      return axios.create({
+        baseURL:
+          // Production Cluster
+          "http://www.ticketing-app.tk",
+        headers: req.headers,
+      });
+    }
   } else {
     // We must be on the browser
     return axios.create({
