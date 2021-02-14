248f85510ce9b61ea76b3dec97c0886ec1f60499
kch-chiu <gh-actions-kch-chiu@github.com>
2021-02-14 17:32:42 -0500

    Refactored api clients

--- client/api/buildOrdersClient.js
+++ client/api/buildOrdersClient.js
@@ -2,18 +2,10 @@ import axios from "axios";
 
 export default ({ req }) => {
   const ordersURL = process.env.NEXT_PUBLIC_ORDERS_URL;
-  console.log(`client-orders-url is: ${ordersURL}`);
-  if (typeof window === "undefined") {
-    // We are on the server
-    return axios.create({
-      // Remote Cluster
-      baseURL: ordersURL,
-      headers: req.headers,
-    });
-  } else {
-    // We must be on the browser
-    return axios.create({
-      baseUrl: "/",
-    });
-  }
+  // We are on the server
+  return axios.create({
+    // Remote Cluster
+    baseURL: ordersURL,
+    headers: req.headers,
+  });
 };
--- client/api/buildTicketsClient.js
+++ client/api/buildTicketsClient.js
@@ -2,18 +2,10 @@ import axios from "axios";
 
 export default ({ req }) => {
   const ticketsURL = process.env.NEXT_PUBLIC_TICKETS_URL;
-  console.log(`client-tickets-url is: ${ticketsURL}`);
-  if (typeof window === "undefined") {
-    // We are on the server
-    return axios.create({
-      // Remote Cluster
-      baseURL: ticketsURL,
-      headers: req.headers,
-    });
-  } else {
-    // We must be on the browser
-    return axios.create({
-      baseUrl: "/",
-    });
-  }
+  // We are on the server
+  return axios.create({
+    // Remote Cluster
+    baseURL: ticketsURL,
+    headers: req.headers,
+  });
 };
