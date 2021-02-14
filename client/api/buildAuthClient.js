ce5004812e3236e7d6037ffce2c685754389d683
kch-chiu <gh-actions-kch-chiu@github.com>
2021-02-14 17:32:42 -0500

    Refactored api clients

--- client/api/buildAuthClient.js
+++ client/api/buildAuthClient.js
@@ -2,18 +2,10 @@ import axios from "axios";
 
 export default ({ req }) => {
   const authURL = process.env.NEXT_PUBLIC_AUTH_URL;
-  console.log(`client-auth-url is: ${authURL}`);
-  if (typeof window === "undefined") {
-    // We are on the server
-    return axios.create({
-      // Remote Cluster
-      baseURL: authURL,
-      headers: req.headers,
-    });
-  } else {
-    // We must be on the browser
-    return axios.create({
-      baseUrl: "/",
-    });
-  }
+  // We are on the server
+  return axios.create({
+    // Remote Cluster
+    baseURL: authURL,
+    headers: req.headers,
+  });
 };
