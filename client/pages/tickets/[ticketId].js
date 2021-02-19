14e5d37fff8fa114ae4834030eb5347199185a44
kch-chiu <gh-actions-kch-chiu@github.com>
2021-02-19 03:30:19 -0500

    Refactored Authentication for client

--- client/pages/tickets/[ticketId].js
+++ client/pages/tickets/[ticketId].js
@@ -1,8 +1,9 @@
 import Router from "next/router";
 import buildClient from "../../api/buildClient";
 import useRequest from "../../hooks/use-request";
+import Header from "../../components/header";
 
-const TicketShow = ({ ticket }) => {
+const TicketShow = ({ currentUser, ticket }) => {
   const ordersRelativeURL = process.env.NEXT_PUBLIC_ORDERS_RELATIVEURL;
   const { doRequest, errors } = useRequest({
     url: `${ordersRelativeURL}`,
@@ -16,24 +17,36 @@ const TicketShow = ({ ticket }) => {
 
   return (
     <div>
-      <h1>{ticket.title}</h1>
-      <h4>Price: {ticket.price}</h4>
-      {errors}
-      <button onClick={() => doRequest()} className="btn btn-primary">
-        Purchase
-      </button>
+      <Header currentUser={currentUser} />
+      <div className="container">
+        <h1>{ticket.title}</h1>
+        <h4>Price: {ticket.price}</h4>
+        {errors}
+        <button onClick={() => doRequest()} className="btn btn-primary">
+          Purchase
+        </button>
+      </div>
     </div>
   );
 };
 
 export const getServerSideProps = async (context) => {
-  const ticketsClient = buildClient(context, 'tickets');
+  const authClient = buildClient(context, "auth");
+  const ticketsClient = buildClient(context, "tickets");
 
+  const authRelativeURL = process.env.NEXT_PUBLIC_AUTH_RELATIVEURL;
   const ticketsRelativeURL = process.env.NEXT_PUBLIC_TICKETS_RELATIVEURL;
-  const { ticketId } = context.query;
-  const { data } = await ticketsClient.get(`${ticketsRelativeURL}${ticketId}`);
 
-  return { props: { ticket: data } };
+  const { data: currentUserData } = await authClient.get(
+    `${authRelativeURL}currentuser`
+  );
+  const { currentUser } = currentUserData;
+  const { ticketId } = context.query;
+  const { data: ticket } = await ticketsClient.get(
+    `${ticketsRelativeURL}${ticketId}`
+  );
+
+  return { props: { currentUser, ticket } };
 };
 
 export default TicketShow;
