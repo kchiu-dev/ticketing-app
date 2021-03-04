778b39241b667ac4d6a10cd437f65a4aa7dae61c
kch-chiu <gh-actions-kch-chiu@github.com>
2021-03-04 04:51:24 -0500

    Updated auth routes and tests

--- auth/src/routes/signup.ts
+++ auth/src/routes/signup.ts
@@ -39,12 +39,7 @@ router.post(
       process.env.JWT_KEY!
     );
 
-    // Store it on session object
-    req.session = {
-      jwt: userJwt,
-    };
-
-    res.status(201).send(user);
+    res.status(201).send(userJwt);
   }
 );
 
--- auth/src/test/setup.ts
+++ auth/src/test/setup.ts
@@ -1,14 +1,5 @@
 import { MongoMemoryServer } from "mongodb-memory-server";
 import mongoose from "mongoose";
-import jwt from "jsonwebtoken";
-
-declare global {
-  namespace NodeJS {
-    interface Global {
-      signin(id?: string): string[];
-    }
-  }
-}
 
 let mongo: any;
 beforeAll(async () => {
@@ -33,26 +24,3 @@ afterAll(async () => {
   await mongo.stop();
   await mongoose.connection.close();
 });
-
-global.signin = () => {
-  // Build a JWT payload. { id, email, iat }
-  const payload = {
-    id: new mongoose.Types.ObjectId().toHexString(),
-    email: "test@test.com",
-  };
-
-  // Create the JWT!
-  const token = jwt.sign(payload, process.env.JWT_KEY!);
-
-  // Build session Object. { jwt: MY_JWT }
-  const session = { jwt: token };
-
-  // Turn that session into JSON
-  const sessionJSON = JSON.stringify(session);
-
-  // Take JSON and encode it as base64
-  const base64 = Buffer.from(sessionJSON).toString("base64");
-
-  // return a string thats the cookie with the encoded data
-  return [`express:sess=${base64}`];
-};
