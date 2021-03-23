79d342e35ec45e1b0cb076ca952fef38ef9fcadc
kch-chiu <70428366+kch-chiu@users.noreply.github.com>
2021-03-22 23:29:09 -0400

    Removed JWT_KEY environment variable from tickets service

--- tickets/src/index.ts
+++ tickets/src/index.ts
@@ -7,9 +7,6 @@ import { OrderCancelledListener } from "./events/listeners/order-cancelled-liste
 const start = async () => {
   console.log("Starting.............");
 
-  if (!process.env.JWT_KEY) {
-    throw new Error("JWT_KEY must be defined");
-  }
   if (!process.env.MONGO_URI) {
     throw new Error("MONGO_URI must be defined");
   }
