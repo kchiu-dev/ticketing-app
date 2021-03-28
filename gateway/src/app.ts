64378a070b4ff7066d336b6cde4d9c0066a97a3e
kch-chiu <70428366+kch-chiu@users.noreply.github.com>
2021-03-28 02:40:01 -0400

    Update app.ts

--- gateway/src/app.ts
+++ gateway/src/app.ts
@@ -2,17 +2,23 @@ import express from "express";
 import { ApolloServer } from "apollo-server-express";
 import { ApolloGateway } from "@apollo/gateway";
 
+import { errorHandler, NotFoundError } from "@kch-chiu/common";
+
 const app = express();
 
 const gateway = new ApolloGateway();
 
 const server = new ApolloServer({
   gateway,
-  subscriptions: false,
-  introspection: true,
-  playground: true,
+  subscriptions: false
 });
 
 server.applyMiddleware({ app });
+
+app.all("*", (_:any, __: any) => {
+  throw new NotFoundError();
+});
+
+app.use(errorHandler);
 
 export { app };
