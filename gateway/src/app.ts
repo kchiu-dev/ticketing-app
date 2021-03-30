8c1bc1eb8c39b8ec090fb127064731995438df46
kch-chiu <70428366+kch-chiu@users.noreply.github.com>
2021-03-30 03:17:11 -0400

    Added GraphQL Playground options

--- gateway/src/app.ts
+++ gateway/src/app.ts
@@ -3,17 +3,27 @@ import cors from "cors";
 import { ApolloServer } from "apollo-server-express";
 import { ApolloGateway } from "@apollo/gateway";
 
+import { errorHandler, NotFoundError } from "@kch-chiu/common";
+
 const app = express();
 
 const gateway = new ApolloGateway();
 
 const server = new ApolloServer({
   gateway,
-  subscriptions: false
+  subscriptions: false,
+  introspection: true,
+  playground: true
 });
 
 app.use(cors());
 
 server.applyMiddleware({ app });
+
+app.all("*", (_: any, __: any) => {
+  throw new NotFoundError();
+});
+
+app.use(errorHandler);
 
 export { app };
--- tickets/src/app.ts
+++ tickets/src/app.ts
@@ -6,22 +6,14 @@ import "graphql-import-node";
 import typeDefs from "../src/graphql/schema.graphql";
 import resolvers from "../src/graphql/resolvers";
 
-import { errorHandler, NotFoundError } from "@kch-chiu/common";
-
 const app = express();
 
 const schema = buildFederatedSchema([{ typeDefs, resolvers }]);
 
-const server = new ApolloServer({ schema, introspection: true, playground: true });
+const server = new ApolloServer({ schema });
 
 app.use(cors());
 
 server.applyMiddleware({ app });
-
-app.all("*", (_:any, __:any) => {
-  throw new NotFoundError();
-})
-
-app.use(errorHandler);
 
 export { app };
