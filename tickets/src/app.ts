69532adf4b13ac5b66dba45842105f7c9b1bece8
kch-chiu <70428366+kch-chiu@users.noreply.github.com>
2021-03-29 23:53:36 -0400

    Added endpoint for tickets

--- tickets/src/app.ts
+++ tickets/src/app.ts
@@ -1,9 +1,12 @@
 import express from "express";
+import cors from "cors";
 import { ApolloServer } from "apollo-server-express";
 import { buildFederatedSchema } from "@apollo/federation";
 import "graphql-import-node";
 import typeDefs from "../src/graphql/schema.graphql";
 import resolvers from "../src/graphql/resolvers";
+
+import { errorHandler, NotFoundError } from "@kch-chiu/common";
 
 const app = express();
 
@@ -11,6 +14,14 @@ const schema = buildFederatedSchema([{ typeDefs, resolvers }]);
 
 const server = new ApolloServer({ schema });
 
+app.use(cors());
+
 server.applyMiddleware({ app });
+
+app.all("*", (_:any, __:any) => {
+  throw new NotFoundError();
+})
+
+app.use(errorHandler);
 
 export { app };
