caef2046e6c3082c9cb1f273b907bc7c2e1b0311
kch-chiu <70428366+kch-chiu@users.noreply.github.com>
2021-03-31 14:24:49 -0400

    Changed port to 4001 for tickets

--- tickets/src/index.ts
+++ tickets/src/index.ts
@@ -15,8 +15,8 @@ const start = async () => {
     console.error(err);
   }
 
-  app.listen(3000, () => {
-    console.log("Listening on port 3000!!!!!!");
+  app.listen(4001, () => {
+    console.log("Listening on port 4001!!!!!!");
   });
 };
 
