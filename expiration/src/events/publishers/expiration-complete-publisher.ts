0549cc9f4488d4c1cdf6787bd4537e1ba1be20bc
kch-chiu <70428366+kch-chiu@users.noreply.github.com>
2021-04-04 03:10:31 -0400

    Removed expiration service

--- expiration/src/events/publishers/expiration-complete-publisher.ts
+++
@@ -1,7 +0,0 @@
-import { ExpirationCompleteEvent, Publisher, Subjects } from "@kch-chiu/common";
-
-export class ExpirationCompletePublisher extends Publisher<
-  ExpirationCompleteEvent
-> {
-  readonly subject = Subjects.ExpirationComplete;
-}
