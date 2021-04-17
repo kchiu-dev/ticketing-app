a95daba537e6f57da685dc6005e64eb0db51b37a
kch-chiu <70428366+kch-chiu@users.noreply.github.com>
2021-04-17 04:19:12 -0400

    Update resolvers.ts

--- orders/src/graphql/resolvers.ts
+++ orders/src/graphql/resolvers.ts
@@ -46,31 +46,27 @@ const resolvers: Resolvers = {
     createOrder: async (_: any, { data }) => {
       const { ticketId } = data;
 
-      console.log(`ticketObjectID is: ${ObjectID.createFromHexString(ticketId)}`);
-      
-      if (!ObjectID.createFromHexString(ticketId)) {
+      const ticket = await getTicketsCollection().findOne({
+        _id: ObjectID.createFromHexString(ticketId),
+      });
+
+      console.log(`ticket is: ${ticket}`);
+
+      if (!ticket) {
         throw new UserInputError("Invalid ticketId");
       }
 
-      try {
-        await getTicketsCollection().findOne({
-          _id: ObjectID.createFromHexString(ticketId),
-        });
+      const dataEntry: Omit<OrderDbObject, "_id"> = {
+        status: "CREATED",
+        ticketId,
+      };
 
-        const dataEntry: Omit<OrderDbObject, "_id"> = {
-          status: "CREATED",
-          ticketId,
-        };
-
-        const document = await getOrdersCollection().insertOne(dataEntry);
-        return fromDbObject({
-          _id: document.insertedId,
-          status: "CREATED",
-          ticketId,
-        });
-      } catch {
-        throw new UserInputError("Invalid ticketId");
-      }
+      const document = await getOrdersCollection().insertOne(dataEntry);
+      return fromDbObject({
+        _id: document.insertedId,
+        status: "CREATED",
+        ticketId,
+      });
     },
     cancelOrder: async (_: any, { orderId }) => {
       try {
