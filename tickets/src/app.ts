2d228ab3a6d941baaab7ae775cebeb9b8a47164b
kch-chiu <70428366+kch-chiu@users.noreply.github.com>
2021-05-01 13:46:16 -0400

    Updated Dgraph schema for tickets and orders

--- tickets/src/app.ts
+++ tickets/src/app.ts
@@ -5,7 +5,7 @@ import "graphql-import-node";
 import * as typeDefs from "../src/graphql/schema.graphql";
 import resolvers from "../src/graphql/resolvers";
 import * as dgraphSchema from "../src/dgraph/dgraph-schema.graphql";
-import { print } from "graphql";
+import { print } from "graphql/language/printer";
 
 const app = express();
 
